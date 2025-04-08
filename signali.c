1c20b1631459cf5e5a8548c467f915b621e6209e
Patrik Pašić <patrik.pasic@fer.hr>
2025-04-08 12:57:25 +0200

    Add files via upload

--- signali.c
+++ signali.c
@@ -4,6 +4,14 @@
 #include <time.h>
 #include <signal.h>
 #include <pthread.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+void sklop_prekid(int sig);
+int max(int arr[]);
+void obrada_pr(int sig);
+void dodaj(int arr[],int sig);
+void obnovi();
 
 int K_Z[5];
 int T_P[5];
@@ -11,12 +19,14 @@ struct clan {
     int T_P[5];
     int p;
     struct clan* sljedeci;
-}
+};
 struct clan* vrh=NULL;
 struct clan* kraj=NULL;
+pthread_t thr_id[2];
+
 void *Glavni(void *x){
     
-    printf("Pokretanje glavne dretve s TID=%ld", (long) gettid());
+    printf("Pokretanje glavne dretve s TID=%ld", (long) syscall(SYS_gettid));
     int i=1;
     while(1){
         printf("Program: iteracija %d\n", i++);
@@ -26,7 +36,7 @@ void *Glavni(void *x){
     return 0;
 }
 void *Sklop(void *x){
-    printf("Pokretanje sklop dretve s TID=%ld", (long) gettid());
+    printf("Pokretanje sklop dretve s TID=%ld", (long) syscall(SYS_gettid));
     //signali
     struct sigaction act;
     act.sa_handler=sklop_prekid;
@@ -38,20 +48,19 @@ void *Sklop(void *x){
     sigaction(SIGILL,&act,NULL);
     sigaction(SIGTRAP,&act,NULL);
     while(1){
+
         if(max(K_Z)>max(T_P)){
             int m=max(K_Z)+1;
             act.sa_handler=obrada_pr;
             sigaction(m,&act,NULL);
 
-        
+            K_Z[m-1]=0;
             pthread_kill(thr_id[0],m);
 
-            act.sa_handler=sklop_prekid;
-            sigaction(m,&act,NULL);
 
             sigset_t pmc;
             sigemptyset(&pmc);
-            sigaddset(&pmc,sig);
+            sigaddset(&pmc,m);
             pthread_sigmask(SIG_UNBLOCK,&pmc,NULL);
         }
     }
@@ -60,7 +69,7 @@ void *Sklop(void *x){
 
 int main() 
 {
-    pthread_t thr_id[2];
+    
     if (pthread_create(&thr_id[0], NULL, Glavni, NULL) != 0) {
         printf("Greska pri stvaranju dretve!\n");
         exit(1);
@@ -76,9 +85,10 @@ int main()
 
 }
 void sklop_prekid(int sig){
+    printf("Signal: %d",sig);
     sigset_t stara_maska;
     K_Z[sig-1]=1;
-    pthread_sigmask(NULL, NULL, &stara_maska);
+    pthread_sigmask(SIG_SETMASK, NULL, &stara_maska);
     sigaddset(&stara_maska,sig);
     pthread_sigmask(SIG_BLOCK,&stara_maska,NULL);
 
@@ -86,16 +96,16 @@ void sklop_prekid(int sig){
 int max(int arr[]){
     int i=0;
     int j=0;
-    for(i;i<6;i++){
+    for(i;i<5;i++){
         if(arr[i]==1){
             j=i;
         }
     }
     return j;
 }
-int obrada_pr(int sig){
+void obrada_pr(int sig){
     dodaj(T_P,sig);
-    T_P[sig-1]=1
+    T_P[sig-1]=1;
     int i=1;
     printf("Pocetak obrade signala %d\n", sig);
     for(i;i<=10;i++){
@@ -104,27 +114,30 @@ int obrada_pr(int sig){
     }
     printf("Kraj obrade signala %d\n", sig);
     obnovi();
+    printf("kraj");
 }
 void dodaj(int arr[],int sig){
-    struct clan clan1;
-    clan1.p=sig;
+    struct clan* clan1=malloc(sizeof(struct clan));
+    clan1->p=sig;
     int i=0;
     for(i;i<5;i++){
-        clan1.T_P[i]=arr[i];
+        clan1->T_P[i]=arr[i];
     }
-    clan1.sljedeci=vrh;
+    clan1->sljedeci=vrh;
+    vrh=clan1;
     if(kraj==NULL){
-        kraj=&clan1;
+        kraj=clan1;
     }
 }
 void obnovi(){
-    struct clan clan1=*vrh;
+    struct clan* clan1=vrh;
     int i=0;
     for(i;i<5;i++){
-        T_P[i]=clan1.T_P[i];
+        T_P[i]=clan1->T_P[i];
     }
     if(vrh->sljedeci==NULL){
         kraj=NULL;
     }
     vrh=vrh->sljedeci;
+    free(clan1);
 }
